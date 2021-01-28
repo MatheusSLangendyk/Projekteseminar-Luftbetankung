07d5751753164bf3293e82ed892c03be52f91769
MatheusSLangendyk <matheus.slangendyk@gmail.com>
2021-01-28 18:08:16 +0100

    Adjustment of Q and R -> Riccatti

--- Code/cost_coupling_control.m
+++ Code/cost_coupling_control.m
@@ -10,14 +10,17 @@ function [F0] = cost_coupling_control(P)
  W = [5;0.01;0;10;0;0;0;0]; %Reference Variable
  A = sys_ol.A;
  B = sys_ol.B;
+ p = size(B,2);
+ n = size(A,1);
  [K_coupling, F_coupling] = coupling_control_scratch(sys_ol,C_tilde,ew_ricati,l,P);
  Ar = A- B*K_coupling;
  if opt_modus == 0 %Conditional Number optimization
     F0 = cond(Ar);
- elseif opt_modus ==1 %Control Variable optimization
+ elseif opt_modus == 1 %Control Variable optimization
      u = F_coupling*W- K_coupling*X_init ;
      F0 = sum(u);
  else
-     F0 = sum(sum(abs(K_coupling))); %Control-Matrix Values optimization
+     F0 =ones(1,p)*abs(K_coupling)*ones(n,1); %Control-Matrix Values optimization
+     
 end
 
