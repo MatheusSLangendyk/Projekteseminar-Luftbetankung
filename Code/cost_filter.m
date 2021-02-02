efd2424f4fa195001af6c490a9328410072b8698
MatheusSLangendyk <matheus.slangendyk@gmail.com>
2021-02-02 15:51:20 +0100

    X_ap filter

--- Code/cost_filter.m
+++ Code/cost_filter.m
@@ -4,7 +4,7 @@ struct_cond = evalin('base','struct_cond ');
 sys_ol = struct_cond.sys_ol;
 C_tilde = struct_cond.C_tilde;
 ew = struct_cond.ew_ricati;
-X_init = struct_cond.X_init;
+X_ap = struct_cond.X_ap;
 W = struct_cond.W;
 P = struct_cond.P;
 t = struct_cond.t ;
@@ -17,8 +17,8 @@ Ar = A- B*K;
 Br = B*F;
 
 sys = ss(Ar,Br,C_tilde,0);
-[Y] = lsim(sys,W,t);
-Y2 = Y(end,l+1:end,X_init);
+[Y] = lsim(sys,W,t,X_ap);
+Y2 = Y(end,l+1:end);
 Q = eye(l);
 F0 = Y2*Q*Y2';
 
